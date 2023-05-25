137aece7234d1ea659e7c7d39884bc9ff2657a90
sabrinalzbh <sachacon15@gmail.com>
2023-05-24 23:03:27 -0400

    added api-google auth-

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,7 @@
-
 import Feed from '@components/Feed';
 
 
-const Home = () => {
-  return (
+const Home = () => (
    <section className="w-full flex-center flex-col">
     <h1 className="head_text text-center">
         Discover & Share
@@ -17,7 +15,7 @@ const Home = () => {
 
     <Feed />
    </section>
-  )
-}
+  );
 
-export default Home
+
+export default Home;
